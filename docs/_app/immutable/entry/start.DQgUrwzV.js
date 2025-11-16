5f5a73a098355196385b8bddf4b6bedfc70f5150
Aayush Bajaj <aayushbajaj7@gmail.com>
2025-11-16 17:24:17 +1100

    enh: rebuild

---
+++ docs/_app/immutable/entry/start.DQgUrwzV.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/DwsQDcvf.js";export{o as load_css,r as start};
---
+++ docs/_app/immutable/nodes/0.DJtMyxbh.js
@@ -0,0 +1,14 @@
+import{d as m,e as y,c as b,a as u,t as T}from"../chunks/BIYvNW0b.js";import{X as S,h as _,c as v,a8 as A,au as E,k as h,n as R,av as k,aw as w,s as $,q as d,b as q,E as O,i as j,ax as N,ag as z,W as g,$ as C,_ as F}from"../chunks/CSOfU5Pt.js";import{h as H}from"../chunks/Ct4oHBmd.js";import"../chunks/DwsQDcvf.js";function I(s,l,a=!1,r=!1,n=!1){var e=s,o="";S(()=>{var c=A;if(o===(o=l()??"")){_&&v();return}if(c.nodes_start!==null&&(E(c.nodes_start,c.nodes_end),c.nodes_start=c.nodes_end=null),o!==""){if(_){h.data;for(var t=v(),p=t;t!==null&&(t.nodeType!==8||t.data!=="");)p=t,t=R(t);if(t===null)throw k(),w;m(h,p),e=$(t);return}var f=o+"";a?f=`<svg>${f}</svg>`:r&&(f=`<math>${f}</math>`);var i=y(f);if((a||r)&&(i=d(i)),m(d(i),i.lastChild),a||r)for(;d(i);)e.before(d(i));else e.before(i)}})}function P(s,l,...a){var r=s,n=N,e;q(()=>{n!==(n=l())&&(e&&(z(e),e=null),e=j(()=>n(r,...a)))},O),_&&(r=h)}const W=!0,G=Object.freeze(Object.defineProperty({__proto__:null,prerender:W},Symbol.toStringTag,{value:"Module"}));var D=T('<meta name="description" content="Explore history through an interactive timeline of influential people and books"> <!>',1);function J(s,l){var a=b();H(n=>{var e=D();C.title="Shrine - Interactive Historical Timeline";var o=F(g(e),2);I(o,()=>`<script type="application/ld+json">
+	{
+		"@context": "https://schema.org",
+		"@type": "WebSite",
+		"name": "Shrine Timeline",
+		"description": "An interactive timeline visualization of historical figures and influential books throughout history",
+		"url": "https://yourdomain.com/shrine/",
+		"potentialAction": {
+			"@type": "SearchAction",
+			"target": "https://yourdomain.com/shrine/search?q={search_term_string}",
+			"query-input": "required name=search_term_string"
+		}
+	}
+	<\/script>`),u(n,e)});var r=g(a);P(r,()=>l.children),u(s,a)}export{J as component,G as universal};
